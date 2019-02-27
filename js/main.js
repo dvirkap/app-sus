e27eb3a6300f824256d7c95c8fee9998a53b258e
dvirkap <37699331+dvirkap@users.noreply.github.com>
2019-02-27 10:24:59 +0200

    nothing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-// import emailApp from './pages/book-app-cmp.js';
+import emailApp from './pages/book-app-cmp.js';
 // import keepApp from './pages/book-app-cmp.js';
 import myRoutes from './routes.js';
-
-const myRouter = new VueRouter({ routes: myRoutes });
-// console.log('myRoutes', myRoutes);
+const myRouter = new VueRouter({routes: myRoutes})
+console.log('myRoutes', myRoutes);
 
 window.vueApp = new Vue({
     el: '#app',
