48c77bc1361774813be0c80ae1e21f488d2fb7ff
dvirkap <37699331+dvirkap@users.noreply.github.com>
2019-02-27 11:07:51 +0200

    working on notes-service

--- js/apps/keep/services/note-service.js
+++ js/apps/keep/services/note-service.js
@@ -1,6 +1,34 @@
-
+import createNoteId from '../../services/utils-service.js'
 export default {
-    template: `
-    <h1>Note service</h1>
-    `,
+    getNotes
 }
+function createDummyNotes(number) {
+for (let i = 0; i < number + 1; i++) {
+    const element = array[i];
+    
+}
+}
+CreateDummyNote()
+
+var gNotes = [
+{
+    "id": createNoteId.makeId(8)
+}
+
+]
+
+    function CreateDummyNote() { 
+        gNotes.push({
+            "id": createNoteId.makeId(8)
+        })    
+
+    }
+
+function getNotes() {
+    console.log(gNotes);
+    return Promise.resolve(gNotes)
+    
+}
+
+
+
