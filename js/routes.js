727f50ecfd504dfe19969be88d3a68d7fd719fbe
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-02-27 10:22:14 +0200

    Update

--- js/routes.js
+++ js/routes.js
@@ -1,14 +1,14 @@
 import homeCmp from './apps/pages/home-cmp.js';
 import aboutCmp from './apps/pages/about-cmp.js';
-// import booksApp from './pages/book-app-cmp.js';
-// import booksAdd from './pages/book-add-cmp.js';
+import emailApp from './apps/email/pages/email-app-cmp.js';
+import keepApp from './apps/keep/pages/keep-app-cmp.js';
 // import bookDetails from './cmps/book-details-cmp.js';
 
 const routes = [
     { path: '/', component: homeCmp },
     { path: '/about', component: aboutCmp },
-    // { path: '/books', component: booksApp },
-    // { path: '/books/:bookId', component: bookDetails },
+    { path: '/email', component: emailApp },
+    { path: '/keep', component: keepApp },
     // { path: '/addbook', component: booksAdd },
 
 ];
