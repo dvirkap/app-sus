7d29dd3d398e2cab575dd46d76341596a2ab0b72
dvirkap <37699331+dvirkap@users.noreply.github.com>
2019-02-28 10:11:28 +0200

    before color picker

--- js/apps/keep/cmps/note-list-cmp.js
+++ js/apps/keep/cmps/note-list-cmp.js
@@ -3,42 +3,35 @@ import notePreview from './note-preview-cmp.js';
 
 export default {
     template: `
-    <section>
-        <h2>User {{ $route.params.id }}</h2>
+   
+        <!-- <h2>User {{ $route.params.id }}</h2> -->
         
-        <ul class="keep-list-container ">
-        <li class="shadow-drop-2-center keep-add-note-container" :class="{'keep-double-size' : isAddNewNoteButtonVisibile}">
-            <div :class="{'keep-hidden' : isRouterViewVisible}" >
+    <ul class="keep-list-container ">
+        <li class="shadow-drop-2-center " :class="{'keep-add-note-container' : isAddNewNoteButtonVisibile}">
+            <div :class="{'keep-hidden' : isRouterViewHidden}" >
                     <router-view></router-view>
             </div>
-                <router-link to="/keep/noteadd">
-                <div :class="{'keep-hidden' : isAddNewNoteButtonVisibile}">
-                <div class="keep-preview-title" @click="addNewNote">Click to add Note</div>
-                <div class="keep-preview-txt">+</div>
-</div>
-                </router-link>
-            </li>
+            <div :class="{'keep-hidden' : isAddNewNoteButtonVisibile}"  @click="addNewNote">
+                    <router-link to="/keep/noteadd">
+                         <div class="keep-preview-title">Click to add Note</div>
+                        <div class="keep-preview-txt">+</div>
+                    </router-link>
+                    </div>
+        </li>
 
-    <li
-        v-for="(currNote, idx) in notes" 
-        :key="currNote.id"
-    >
-        <router-link
-        :to="'/keep/' + currNote.id">
-            <note-preview
-            :note="currNote" :idx="idx+1"
-            >
-            </note-preview>
-        </router-link>
-    </li>
-        </ul>
-    </section>
+        <li v-for="(currNote, idx) in notes" :key="currNote.id">
+            <router-link :to="'/keep/' + currNote.id">
+                <note-preview :note="currNote" :idx="idx+1"></note-preview>
+            </router-link>
+        </li>
+    </ul>
+    
     `,
 
     data() {
         return {
             notes: [],
-            isRouterViewVisible: false,
+            isRouterViewHidden: true,
             isAddNewNoteButtonVisibile: false,
             isAddNoteActivated: true,
 
@@ -48,7 +41,7 @@ export default {
     },
     methods: {
         addNewNote() {
-            this.isRouterViewVisible = false;
+            this.isRouterViewHidden = false;
             this.isAddNewNoteButtonVisibile = true;
         }
     },
