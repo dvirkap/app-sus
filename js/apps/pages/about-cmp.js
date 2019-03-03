73ec0f39b7f391791540f075d520c13d7b18c38d
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-03-03 18:26:05 +0200

    about-cmp.js

--- js/apps/pages/about-cmp.js
+++ js/apps/pages/about-cmp.js
@@ -1,12 +1,31 @@
 var interval;
 
 export default {
-    template: `<h1>About Us</h1>`,
+    template: `
+        <section>
+            <div class="about-main flex">
+                    <!-- <h1>About Us</h1> -->
+                <div>
+                    <h3>Dvir Kaplan</h3>
+
+                    <h3><i class="fas fa-at"></i> dvir.kaplan@appsus.com</h3>
+                    <h3>Hobbies: Hiking, Css flexing</h3>
+                    <img src="https://api.adorable.io/avatars/285/>Dvir Kaplan.png"><img>
+                </div>
+                <div>
+                    <h3>Nir Fuchs</h3>
+                    <h3><i class="fas fa-at"></i> nir.fuchs@appsus.com</h3>
+                    <h3>Hobbies: Ski, Vue.js routing</h3>
+                    <img src="https://api.adorable.io/avatars/285/Nir Fuchs.png"><img>
+                </div>
+            </div>
+        </section>
+        `,
+
     created() {
-        interval = setInterval(() => { console.log('About tab') }, 1000);
+
     },
     destroyed() {
-        console.log('Exit About tab');
-        clearInterval(interval);
+
     }
 }
