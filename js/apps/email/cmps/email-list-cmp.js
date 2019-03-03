a725b9838ff0e565b9d749150e25ed460201ce72
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-03-03 15:09:08 +0200

    Fix style

--- js/apps/email/cmps/email-list-cmp.js
+++ js/apps/email/cmps/email-list-cmp.js
@@ -3,13 +3,24 @@ import emailPreview from './email-preview-cmp.js';
 export default {
     props: ['emails'],
     template: `
+    <section class="email-list flex">
+        <router-link class="email-list-row flex" v-for="(currEmail, idx) in emails" :key="currEmail.id" :to="'/email/' + currEmail.id">
+            <!-- <div class="email-list-row flex"> -->
+                <email-preview class="email-list-preview" v-bind:email="currEmail"></email-preview>
+                <div class="email-list-buttons flex">
+                    <button class="email-list-btn-delete" v-on:click.stop.prevent="onDeleteEmail(currEmail)"></button>
+                    <button v-show="!currEmail.isRead" class="email-list-btn-unread" v-on:click.stop.prevent="onToggleIsRead(currEmail)"></button>
+                    <button v-show="currEmail.isRead" class="email-list-btn-read" v-on:click.stop.prevent="onToggleIsRead(currEmail)"></button>
+                </div>
+            <!-- </div> -->
+        </router-link>
+    </section>
         <!-- <section> -->
             <!-- <div>Email List</div> -->
-            <div class="email-list">
+            <!-- <div class="email-list">
                 <table border="1">
                     <tbody>
                         <router-link class="flex" v-for="(currEmail, idx) in emails" :key="currEmail.id" :to="'/email/' + currEmail.id">
-                        <!-- <input class="checkboxes" type="checkbox" id="idx" name="idx" v-on:click.stop="checkboxClicked"></input> -->
                             <tr>
                                 <td nowrap class="email-list-td">
                                     <email-preview v-bind:email="currEmail">
@@ -26,7 +37,7 @@ export default {
                         </router-link>
                     </tbody>
                 </table>
-            </div>
+            </div> -->
             
             <!--<div class="email-list flex">
                 <email-preview v-for="(currEmail, idx) in emails" v-bind:email="currEmail" :key="currEmail.id" v-on:click.native="selectEmail(currEmail)">
